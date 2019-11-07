130a3d2566d841194ba9e4f36aeeeaad040efa70
unknown <stephon.savic@gmail.com>
2019-11-07 13:38:14 +0100

    added code for arexx

---
+++ arexx/arexx.ino
@@ -0,0 +1,149 @@
+#include <AARMotor.h>
+
+uint8_t Speed = 100;
+uint8_t EchoPin = 8;
+uint8_t TrigPin = 4;
+uint16_t Time_Echo_us = 0;
+uint16_t Len_mm_X100 = 0;
+uint16_t Len_Integer = 0; //
+uint16_t Len_Fraction = 0;
+uint8_t Lag = 0;
+
+volatile int16_t RightRevolutions = 0;
+volatile int16_t LeftRevolutions = 0;
+uint32_t LastMovement = 0;
+uint32_t LastLeftMillis = 0;
+uint32_t LastRightMillis = 0;
+int8_t RightIncrement = 0;
+int8_t LeftIncrement = 0;
+
+AARMotorClass AarMotor;
+
+void pciSetup(byte pin)
+{
+    *digitalPinToPCMSK(pin) |= bit (digitalPinToPCMSKbit(pin));  // enable pin
+    PCIFR  |= bit (digitalPinToPCICRbit(pin)); // clear any outstanding interrupt
+    PCICR  |= bit (digitalPinToPCICRbit(pin)); // enable interrupt for the group
+}
+ 
+ 
+ISR (PCINT1_vect) // handle pin change interrupt for A0 to A5 here
+ {
+    uint8_t currA0;
+    uint8_t currA1;
+    uint32_t timestamp = millis();
+    uint32_t leftDiff = timestamp - LastLeftMillis;
+    uint32_t rightDiff = timestamp - LastRightMillis;
+    
+    currA0 = digitalRead(A0);
+    currA1 = digitalRead(A1);
+
+    if (currA0){
+        LeftRevolutions += LeftIncrement;
+        LastLeftMillis = timestamp;
+    }
+
+    if (currA1){
+        RightRevolutions += RightIncrement;
+        LastRightMillis = timestamp;
+    }
+ }
+
+void setup() { 
+  // Initialize AAR motor
+  AarMotor.Init(Speed);
+  // Initialize serial port
+  Serial.begin(9600);
+  // Initialize pins for ultrasonic sensor
+  pinMode(EchoPin, INPUT);
+  pinMode(TrigPin, OUTPUT);
+  // Initialize pins for odometry
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+  pciSetup(A0);
+  pciSetup(A1);
+}
+
+void calculateDistance() {
+  digitalWrite(TrigPin, HIGH);
+  delayMicroseconds(50);
+  digitalWrite(TrigPin, LOW);
+  Time_Echo_us = pulseIn(EchoPin, HIGH);
+  Len_mm_X100 = (Time_Echo_us * 34) / 2;
+  Len_Integer = Len_mm_X100 / 100;
+  Len_Fraction = Len_mm_X100 % 100;
+}
+
+void printInfo(){
+  // Print distance  
+  Serial.print(Len_Integer, DEC);
+  Serial.print(".");
+  Serial.print(Len_Fraction, DEC);
+  Serial.print(';');
+  // Print revolutions
+  Serial.print(LeftRevolutions, DEC);
+  Serial.print(';');
+  Serial.print(RightRevolutions, DEC);
+  Serial.print(';');
+  // Print speed
+  Serial.print(Speed, DEC);
+  Serial.println();
+}
+
+void loop() {
+  int8_t cmd = 0;
+  // put your main code here, to run repeatedly:
+  if (Serial.available() > 0) {
+    // read the incoming bytes:
+    cmd = Serial.read();
+    switch (cmd) {
+      case 'w':
+      case 'W':
+        RightIncrement = 1;
+        LeftIncrement = 1;
+        LastMovement = millis();
+        AarMotor.Forward(0, Speed);
+        break;
+      case 's':
+      case 'S':
+        RightIncrement = -1;
+        LeftIncrement = -1;
+        LastMovement = millis();
+        AarMotor.Backward(0, Speed);
+        break;
+      case 'd':
+      case 'D':
+        RightIncrement = -1;
+        LeftIncrement = 1;
+        LastMovement = millis();
+        AarMotor.TurnOnSpotRight(0, Speed);
+        break;
+      case 'a':
+      case 'A':
+        RightIncrement = 1;
+        LeftIncrement = -1;
+        LastMovement = millis();
+        AarMotor.TurnOnSpotLeft(0, Speed);
+        break;
+      case 'q':
+      case 'Q':
+        Speed = Speed == 250 ? Speed : Speed + 25;
+        break;
+      case 'e':
+      case 'E':
+        Speed = Speed == 100 ? Speed : Speed - 25;
+        break;
+      case 'i':
+      case 'I':
+        calculateDistance();
+        printInfo();
+        break;        
+    }
+  }
+
+  if (millis() - LastMovement >= 500){
+      AarMotor.Stop();
+      RightIncrement = 0;
+      LeftIncrement = 0;
+  }
+}
